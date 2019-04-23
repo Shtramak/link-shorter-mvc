ac9888ea586d66ffea5e70deba57246e7e580d46
shtramak <shtramak@gmail.com>
2019-04-23 19:21:50 +0300

    add AppConfig.java, add its import to WebConfig and that is wrong :(

--- src/main/java/shorter/WebConfig.java
+++ src/main/java/shorter/WebConfig.java
@@ -1,22 +1,33 @@
 package shorter;
 
-import shorter.controller.HelloController;
-import web.MyController;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import shorter.controller.FullController;
+import shorter.controller.HelloController;
+import shorter.controller.ShorterController;
+import web.MyController;
 
 @Configuration
+@Import(AppConfig.class)
 public class WebConfig {
 
-	@Bean
-	public MyController hello() {
-		return new HelloController();
-	}
+    @Bean
+    public MyController hello() {
+        return new HelloController();
+    }
 
-	//TODO: create ShorterController bean to make short link from full
-	//req examp: http://localhost:8080/shorter/short?full=http://jug.ua
+    //TODO: create ShorterController bean to make short link from full
+    //req examp: http://localhost:8080/shorter/short?full=http://jug.ua
+    @Bean
+    public FullController fullLink() {
+        return new FullController();
+    }
 
-	//TODO: create FullController bean to make full link from short
-	//req examp: http://localhost:8080/shorter/full?short=http://asdsa.sh
+    //TODO: create FullController bean to make full link from short
+    //req examp: http://localhost:8080/shorter/full?short=http://asdsa.sh
+    @Bean
+    public ShorterController shortLink() {
+        return new ShorterController();
+    }
 }
